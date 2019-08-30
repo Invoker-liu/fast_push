4d069cfb15662d3683bfdb8be9214698888e00a7
liu_jianjun <liujianjun@xiniaoyun.com>
2019-08-30 15:42:51 +0800

    增加注释等

--- push.py
+++ push.py
@@ -6,7 +6,14 @@ def git(*args):
     return subprocess.check_call(['git'] + list(args))
 
 
-# 合并南昌GIT
+# git clone --bare http://XXX.git
+# git push --mirror http://XXXXXcopy.git
+# git remote add upstream http://XXX.git
+# git fetch upstream
+# git checkout master
+# git merge upstream/master
+# git push origin master
+# 基于GitUpStream的功能对于GIT在本地进行一次转发,方便进行急速同步
 os.chdir('D:/clone/ddp-decision-service')
 git("status")
 git("fetch", "upstream")
