34d1c401622f0b37b0a7f9c53757e1137f1456f9
liu_jianjun <oppo5737>
2020-01-02 10:56:17 +0800

    使用轮训方式进行GIT检查

--- push.py
+++ push.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import time
 
 
 def git(*args):
@@ -14,9 +15,15 @@ def git(*args):
 # git merge upstream/master
 # git push origin master
 # 基于GitUpStream的功能对于GIT在本地进行一次转发,方便进行急速同步
-os.chdir('D:/clone/ddp-decision-service')
-git("status")
-git("fetch", "upstream")
-git("checkout", "master")
-git("merge", "upstream/master")
-git("push", "origin", "master")
+def loop_git_check():
+    while True:
+        os.chdir('D:/clone/ddp-decision-service')
+        git("status")
+        git("fetch", "upstream")
+        git("checkout", "develop")
+        git("merge", "upstream/develop")
+        git("push", "origin", "develop")
+        time.sleep(5)
+
+
+loop_git_check()
